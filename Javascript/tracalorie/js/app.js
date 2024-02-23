fa7783ede10e7a7659d222b3822fd8e3aad44d33
Arth Limchiu <limchiuarth@gmail.com>
2024-02-23 20:10:16 +0800

    Add support for filtering meals and workouts and resetting day

--- Javascript/tracalorie/js/app.js
+++ Javascript/tracalorie/js/app.js
@@ -47,6 +47,13 @@ class CalorieTracker {
         }
     }
 
+    reset() {
+        this._totalCalories = 0;
+        this._meals = [];
+        this._workouts = [];
+        this._render();
+    }
+
     _displayCaloriesTotal() {
         const totalCaloriesEl = document.querySelector('#calories-total');
         totalCaloriesEl.innerHTML = this._totalCalories;
@@ -185,6 +192,18 @@ class App {
         document
             .querySelector('#workout-items')
             .addEventListener('click', this._removeItem.bind(this, 'workout'));
+
+        document
+            .querySelector('#filter-meals')
+            .addEventListener('keyup', this._filterItems.bind(this, 'meal'));
+
+        document
+            .querySelector('#filter-workouts')
+            .addEventListener('keyup', this._filterItems.bind(this, 'workout'));
+        
+        document
+            .querySelector('#reset')
+            .addEventListener('click', this._reset.bind(this));
     }
 
     _newItem(type, e) {
@@ -222,6 +241,26 @@ class App {
                 e.target.closest('.card').remove();
             }
         }
+    }
+
+    _filterItems(type, e) {
+        const text = e.target.value.toLowerCase();
+        document.querySelectorAll(`#${type}-items .card`).forEach((item) => {
+            const name = item.firstElementChild.firstElementChild.textContent;
+            if (name.toLowerCase().indexOf(text) !== -1) {
+                item.style.display = 'block';
+            } else {
+                item.style.display = 'none';
+            }
+        });
+    }
+
+    _reset() {
+        this._tracker.reset();
+        document.querySelector('#meal-items').innerHTML = '';
+        document.querySelector('#workout-items').innerHTML = '';
+        document.querySelector('#filter-meals').value = '';
+        document.querySelector('#filter-workouts').value = '';
     }
 }
 
