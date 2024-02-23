0884608e4cc7330c7a443445ba2c3ff3503094b9
Arth Limchiu <limchiuarth@gmail.com>
2024-02-23 19:51:16 +0800

    Add support for removing meal and workouts to list

--- Javascript/tracalorie/js/app.js
+++ Javascript/tracalorie/js/app.js
@@ -27,6 +27,26 @@ class CalorieTracker {
         this._render();
     }
 
+    removeMeal(id) {
+        const index = this._meals.findIndex((meal) => meal.id === id);
+        if (index !== -1) {
+            const meal = this._meals[index];
+            this._totalCalories -= meal.calories;
+            this._meals.splice(index, 1);
+            this._render();
+        }
+    }
+
+    removeWorkout(id) {
+        const index = this._workouts.findIndex((workout) => workout.id === id);
+        if (index !== -1) {
+            const workout = this._workouts[index];
+            this._totalCalories += workout.calories;
+            this._workouts.splice(index, 1);
+            this._render();
+        }
+    }
+
     _displayCaloriesTotal() {
         const totalCaloriesEl = document.querySelector('#calories-total');
         totalCaloriesEl.innerHTML = this._totalCalories;
@@ -150,8 +170,21 @@ class App {
     constructor() {
         this._tracker = new CalorieTracker();
         
-        document.querySelector('#meal-form').addEventListener('submit', this._newItem.bind(this, 'meal'));
-        document.querySelector('#workout-form').addEventListener('submit', this._newItem.bind(this, 'workout'));
+        document
+            .querySelector('#meal-form')
+            .addEventListener('submit', this._newItem.bind(this, 'meal'));
+
+        document
+            .querySelector('#workout-form')
+            .addEventListener('submit', this._newItem.bind(this, 'workout'));
+
+        document
+            .querySelector('#meal-items')
+            .addEventListener('click', this._removeItem.bind(this, 'meal'));
+
+        document
+            .querySelector('#workout-items')
+            .addEventListener('click', this._removeItem.bind(this, 'workout'));
     }
 
     _newItem(type, e) {
@@ -179,6 +212,16 @@ class App {
 
         const collapseItem = document.querySelector(`#collapse-${type}`);
         const bsCollapse = new bootstrap.Collapse(collapseItem, { toggle: true });
+    }
+
+    _removeItem(type, e) {
+        if (e.target.classList.contains('delete') || e.target.classList.contains('fa-xmark')) {
+            if (confirm('Are you sure?')) {
+                const id = e.target.closest('.card').getAttribute('data-id');
+                type === 'meal' ? this._tracker.removeMeal(id) : this._tracker.removeWorkout(id);
+                e.target.closest('.card').remove();
+            }
+        }
     }
 }
 
