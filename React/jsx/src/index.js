79bc5a6e066845da99cfc2dae82e081437db2f13
Arth Limchiu <limchiuarth@gmail.com>
2024-02-27 19:28:24 +0800

    Customizing Elements with Props

---
+++ React/jsx/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const el = document.getElementById('root');
+
+const root = ReactDOM.createRoot(el);
+
+function App() {
+    const inputType = 'number';
+    const minValue = 5;
+    return <input type={inputType} min={minValue} />;
+}
+
+root.render(<App />);
