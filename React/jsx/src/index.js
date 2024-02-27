3c12479d79ac7f53ebdb52e9553d3d92b4a971fc
Arth Limchiu <limchiuarth@gmail.com>
2024-02-27 19:50:22 +0800

    Extracting Components

--- React/jsx/src/index.js
+++ React/jsx/src/index.js
@@ -1,16 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
 
 const el = document.getElementById('root');
 
 const root = ReactDOM.createRoot(el);
-
-function App() {
-    return <div style={{ textDecoration: 'none', padding: '5px'}} />;
-    // return <div className="divider" />;
-    // return <input spellCheck />;
-    // return <input spellCheck={false} />;
-    // return <input maxLength={5} />;
-}
 
 root.render(<App />);
