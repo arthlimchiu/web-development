38d607ae0fb4f45e513ebad0487bf26a513ba0c8
Arth Limchiu <limchiuarth@gmail.com>
2024-02-28 20:32:14 +0800

    Integrate css styling to profile cards

--- React/pdas/src/App.js
+++ React/pdas/src/App.js
@@ -9,9 +9,21 @@ function App() {
     <div>
       <div>Personal Digital Assistants</div>
 
-      <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
-      <ProfileCard title="Cortana" handle="@cortana22" image={CortanaImage} />
-      <ProfileCard title="Siri" handle="@siri55" image={SiriImage} />
+      <div className="container">
+        <section className="section">
+          <div className="columns">
+            <div className="column is-4">
+              <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
+            </div>
+            <div className="column is-4">
+              <ProfileCard title="Cortana" handle="@cortana22" image={CortanaImage} />
+            </div>
+            <div className="column is-4">
+              <ProfileCard title="Siri" handle="@siri55" image={SiriImage} />
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
