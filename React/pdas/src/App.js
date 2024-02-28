e1fcdf1930f424c793decfe516140f4a5d72cb10
Arth Limchiu <limchiuarth@gmail.com>
2024-02-28 20:02:39 +0800

    Including images

--- React/pdas/src/App.js
+++ React/pdas/src/App.js
@@ -1,13 +1,16 @@
-import ProfileCard from "./ProfileCard";
+import ProfileCard from './ProfileCard';
+import AlexaImage from './images/alexa.png';
+import CortanaImage from './images/cortana.png';
+import SiriImage from './images/siri.png';
 
 function App() {
   return(
     <div>
       <div>Personal Digital Assistants</div>
 
-      <ProfileCard title="Alex" handle="@alexa99" />
-      <ProfileCard title="Cortana" handle="@cortana22" />
-      <ProfileCard title="Siri" handle="@siri55" />
+      <ProfileCard title='Alex' handle='@alexa99' />
+      <ProfileCard title='Cortana' handle='@cortana22' />
+      <ProfileCard title='Siri' handle='@siri55' />
     </div>
   );
 }
