de356fca728fd557a814e8a1ab90248f90dfa799
Arth Limchiu <limchiuarth@gmail.com>
2024-02-29 20:15:21 +0800

    Intro to state system

--- React/animals/src/App.js
+++ React/animals/src/App.js
@@ -1,11 +1,14 @@
 function App() {
+    const [count, setCount] = useState(0);
+
     const handleClick = () => {
-        console.log('Button was clicked!');
+        setCount(count + 1);
     };
 
     return (
         <div>
             <button onClick={handleClick}>Add Animal</button>
+            <div>Number of animals: {count}</div>
         </div>
     );
 }
