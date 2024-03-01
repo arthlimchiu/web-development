1c06ca9b0f781901062a418b74106811c962eed1
Arth Limchiu <limchiuarth@gmail.com>
2024-03-01 21:14:00 +0800

    Implement child to parent communication using props system

--- React/pics/src/api.js
+++ React/pics/src/api.js
@@ -1,16 +1,14 @@
 import axios from 'axios';
 
-const searchImages = async () => {
+const searchImages = async (term) => {
     const response = await axios.get('https://api.unsplash.com/search/photos', {
         headers: {
             Authorization: 'Client-ID qMbSvItaJNziWlfsfcpxJV7tLgLzggK4SwpfemBSlSM'
         },
         params: {
-            query: 'cars'
+            query: term
         }
     });
-
-    console.log(response);
 
     return response;
 };
--- React/pics/src/index.js
+++ React/pics/src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.js';
-import searchImages from './api.js';
-
-searchImages();
 
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
