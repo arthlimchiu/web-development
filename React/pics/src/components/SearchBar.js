9bc60c98f5d0e3eadfc9169a7ff6ffa33b4e35fc
Arth Limchiu <limchiuarth@gmail.com>
2024-03-05 19:34:58 +0800

    Integrate images from response to UI list

--- React/pics/src/components/SearchBar.js
+++ React/pics/src/components/SearchBar.js
@@ -1,12 +1,22 @@
+import './SearchBar.css';
+import { useState } from 'react';
+
 function SearchBar({ onSubmit }) {
-    const handleClick = () => {
-        onSubmit('cars');
+    const [term, setTerm] = useState('');
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        onSubmit(term);
+    };
+    const handleChange = (e) => {
+        setTerm(e.target.value);
     };
 
     return (
-        <div>
-            <input />
-            <button onClick={handleClick}>Click me</button>
+        <div className="search-bar">
+            <form onSubmit={handleFormSubmit}>
+                <label>Enter Search Term</label>
+                <input value={term} onChange={handleChange} />
+            </form>
         </div>
     );
 }
