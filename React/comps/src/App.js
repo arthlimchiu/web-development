9cfea63071cffbca326961530f6ddc43227859ca
Arth Limchiu <limchiuarth@gmail.com>
2024-03-13 19:44:11 +0800

    Add state update for selected option in dropdown component

--- React/comps/src/App.js
+++ React/comps/src/App.js
@@ -1,11 +1,13 @@
+import { useState } from 'react';
 import Dropdown from './components/Dropdown';
 
 export default function App() {
+    const [selectedOption, setSelectedOption] = useState(null);
     const options = [
         { label: 'Red', value: 'red'},
         { label: 'Green', value: 'green'},
         { label: 'Blue', value: 'blue'},
     ];
 
-    return (<Dropdown options={options} />);    
+    return (<Dropdown options={options} selectedOption={selectedOption} onSelect={setSelectedOption} />);    
 }
--- React/comps/src/components/Dropdown.js
+++ React/comps/src/components/Dropdown.js
@@ -1,13 +1,13 @@
 import { useState } from 'react';
 
-export default function Dropdown({ options }) {
+export default function Dropdown({ options, selectedOption, onSelect }) {
     const [isOpen, setIsOpen] = useState(false);
     const handleClick = () => {
         setIsOpen(!isOpen);
     };
     const handleOptionClick = (option) => {
         setIsOpen(false);
-        console.log(option);
+        onSelect(option);
     };
 
     const renderedOptions = options.map((option) => {
@@ -18,7 +18,7 @@ export default function Dropdown({ options }) {
 
     return (
         <div>
-            <div onClick={handleClick}>Select...</div>
+            <div onClick={handleClick}>{selectedOption ? selectedOption.label : 'Select...'}</div>
             {isOpen && <div>{renderedOptions}</div>}
         </div>
     );
