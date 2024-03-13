44162c92b35a28163b868f71f5a1fe76ee1eaadf
Arth Limchiu <limchiuarth@gmail.com>
2024-03-13 13:12:03 +0800

    Add conditional rendering support for Accordion component

--- React/comps/src/App.js
+++ React/comps/src/App.js
@@ -3,14 +3,17 @@ import Accordion from './components/Accordion';
 export default function App() {
     const items = [
         {
+            id: '21fsa',
             label: 'Can I use React on a project?',
             content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mattis turpis vel ex vulputate consequat. Cras sed imperdiet tortor. Donec vulputate accumsan varius. Vivamus metus tortor, vehicula nec lacus sed, porta pulvinar nisi. Ut euismod cursus blandit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Ut tempor nunc pellentesque porta auctor.'
         },
         {
+            id: 'asdg2',
             label: 'Can I use React on a project?',
             content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mattis turpis vel ex vulputate consequat. Cras sed imperdiet tortor. Donec vulputate accumsan varius. Vivamus metus tortor, vehicula nec lacus sed, porta pulvinar nisi. Ut euismod cursus blandit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Ut tempor nunc pellentesque porta auctor.'
         },
         {
+            id: 'kjgh6',
             label: 'Can I use React on a project?',
             content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mattis turpis vel ex vulputate consequat. Cras sed imperdiet tortor. Donec vulputate accumsan varius. Vivamus metus tortor, vehicula nec lacus sed, porta pulvinar nisi. Ut euismod cursus blandit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Ut tempor nunc pellentesque porta auctor.'
         }
--- React/comps/src/components/Accordion.js
+++ React/comps/src/components/Accordion.js
@@ -1,5 +1,20 @@
+import { useState } from "react";
+
 export default function Accordion({ items }) {
+    const [expandedIndex, setExpandedIndex] = useState(0);
+
+    const renderedItems = items.map((item, index) => {
+        const isExpanded = index === expandedIndex;
+
+        return (
+            <div key={item.id}>
+                <div>{item.label}</div>
+                {isExpanded && <div>{item.content}</div>}
+            </div>
+        );
+    });
+
     return (
-        <div>Accordion</div>
+        <div>{renderedItems}</div>
     );
 }
