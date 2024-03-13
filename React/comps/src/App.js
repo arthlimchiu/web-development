35a8429385d5c878197164e559fb4291bb4b78ba
Arth Limchiu <limchiuarth@gmail.com>
2024-03-14 07:08:34 +0800

    Clear App js

--- React/comps/src/App.js
+++ React/comps/src/App.js
@@ -1,22 +1,7 @@
 import { useState } from 'react';
-import Dropdown from './components/Dropdown';
 
 export default function App() {
-    const [selectedOption, setSelectedOption] = useState(null);
-    const options = [
-        { label: 'Red', value: 'red'},
-        { label: 'Green', value: 'green'},
-        { label: 'Blue', value: 'blue'},
-    ];
-
-    const onChangeSelectedOption = (newOption) => {
-        setSelectedOption(newOption);
-    };
-
     return (
-        <div className="flex">
-            <Dropdown options={options} value={selectedOption} onChange={onChangeSelectedOption} />
-            <Dropdown options={options} value={selectedOption} onChange={onChangeSelectedOption} />
-        </div>
+        <div>App</div>
     );
 }
