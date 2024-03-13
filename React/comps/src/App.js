6812a4990135dfa44b999fb7de57da0b7b46d637
Arth Limchiu <limchiuarth@gmail.com>
2024-03-13 12:42:18 +0800

    Create accordion component

--- React/comps/src/App.js
+++ React/comps/src/App.js
@@ -1,8 +1,22 @@
-import ButtonPage from './pages/ButtonPage';
+import Accordion from './components/Accordion';
 
 export default function App() {
+    const items = [
+        {
+            label: 'Can I use React on a project?',
+            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mattis turpis vel ex vulputate consequat. Cras sed imperdiet tortor. Donec vulputate accumsan varius. Vivamus metus tortor, vehicula nec lacus sed, porta pulvinar nisi. Ut euismod cursus blandit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Ut tempor nunc pellentesque porta auctor.'
+        },
+        {
+            label: 'Can I use React on a project?',
+            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mattis turpis vel ex vulputate consequat. Cras sed imperdiet tortor. Donec vulputate accumsan varius. Vivamus metus tortor, vehicula nec lacus sed, porta pulvinar nisi. Ut euismod cursus blandit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Ut tempor nunc pellentesque porta auctor.'
+        },
+        {
+            label: 'Can I use React on a project?',
+            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mattis turpis vel ex vulputate consequat. Cras sed imperdiet tortor. Donec vulputate accumsan varius. Vivamus metus tortor, vehicula nec lacus sed, porta pulvinar nisi. Ut euismod cursus blandit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Ut tempor nunc pellentesque porta auctor.'
+        }
+    ];
 
     return (
-        <ButtonPage />
+        <Accordion items={items} />
     );
 }
---
+++ React/comps/src/components/Accordion.js
@@ -0,0 +1,5 @@
+export default function Accordion({ items }) {
+    return (
+        <div>Accordion</div>
+    );
+}
