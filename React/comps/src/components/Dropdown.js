8ffed0e7942b43752a6b9fbb77ddadac435f5f9c
Arth Limchiu <limchiuarth@gmail.com>
2024-03-13 19:38:01 +0800

    Add event handlers

--- React/comps/src/components/Dropdown.js
+++ React/comps/src/components/Dropdown.js
@@ -1,5 +1,25 @@
+import { useState } from 'react';
+
 export default function Dropdown({ options }) {
+    const [isOpen, setIsOpen] = useState(false);
+    const handleClick = () => {
+        setIsOpen(!isOpen);
+    };
+    const handleOptionClick = (option) => {
+        setIsOpen(false);
+        console.log(option);
+    };
+
+    const renderedOptions = options.map((option) => {
+        return (
+            <div onClick={() => handleOptionClick(option)} key={option.value}>{option.label}</div>
+        );
+    });
+
     return (
-        <div>Dropdown...</div>
+        <div>
+            <div onClick={handleClick}>Select...</div>
+            {isOpen && <div>{renderedOptions}</div>}
+        </div>
     );
 }
