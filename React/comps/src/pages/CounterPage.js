665c2ed841efdb18c8ef2a30ffe2aa7a1fbde23f
Arth Limchiu <limchiuarth@gmail.com>
2024-03-20 06:54:08 +0800

    Add initial implementation of useReducer

--- React/comps/src/pages/CounterPage.js
+++ React/comps/src/pages/CounterPage.js
@@ -1,15 +1,56 @@
+import { useReducer } from 'react';
 import Button from '../components/Button';
-import useCounter from '../hooks/use-counter';
+import Panel from '../components/Panel';
+
+const reducer = (state, action) => {
+    return {
+        ...state,
+        count: state.count + 1
+    };
+};
 
 export default function CounterPage({ initialCount }) {
-    const { count, increment } = useCounter(initialCount);
+    const [state, dispatch] = useReducer(reducer, {
+        count: initialCount,
+        valueToAdd: 0
+    });
+
+    const increment = () => {
+        dispatch();
+    };
+
+    const decrement = () => {
+        // setCount(count - 1);
+    };
+
+    const handleChange = (event) => {
+        const value = parseInt(event.target.value) || 0;
+        // setValueToAdd(value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // setCount(count + valueToAdd)
+        // setValueToAdd(0);
+    };
 
     return (
-        <div>
-            <h1>Count is {count}</h1>
-            <Button primary onClick={increment}>
-                Increment
-            </Button>
-        </div>
+        <Panel className="m-3">
+            <h1 className="text-lg">Count is {state.count}</h1>
+            <div className="flex flex-row">
+                <Button primary outline onClick={increment}>
+                    Increment
+                </Button>
+                <Button primary outline onClick={decrement}>
+                    Decrement
+                </Button>
+            </div>
+
+            <form onSubmit={handleSubmit}>
+                <label>Add a lot!</label>
+                <input value={state.valueToAdd || ""} onChange={handleChange} type="number" className="p-1 m-3 bg-gray-50 border border-gray-300" />
+                <Button outline>Add it!</Button>
+            </form>
+        </Panel>
     );
 }
