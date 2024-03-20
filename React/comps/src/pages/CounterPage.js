60abc96637dd308740c087617f807d3f1aba2220
Arth Limchiu <limchiuarth@gmail.com>
2024-03-20 19:11:05 +0800

    Add action types for count page's reducer

--- React/comps/src/pages/CounterPage.js
+++ React/comps/src/pages/CounterPage.js
@@ -2,11 +2,37 @@ import { useReducer } from 'react';
 import Button from '../components/Button';
 import Panel from '../components/Panel';
 
+const INCREMENT_COUNT = 'increment';
+const DECREMENT_COUNT = 'decrement';
+const SET_VALUE_TO_ADD = 'set-value-to-add';
+const ADD_VALUE_TO_COUNT = 'add-value-to-count';
+
 const reducer = (state, action) => {
-    return {
-        ...state,
-        count: state.count + 1
-    };
+    switch (action.type) {
+        case INCREMENT_COUNT:
+            return {
+                ...state,
+                count: state.count + 1
+            };
+        case DECREMENT_COUNT:
+            return {
+                ...state,
+                count: state.count - 1
+            };
+        case SET_VALUE_TO_ADD:
+            return {
+                ...state,
+                valueToAdd: action.payload
+            };
+        case ADD_VALUE_TO_COUNT:
+            return {
+                ...state,
+                count: state.count + state.valueToAdd,
+                valueToAdd: 0
+            };
+        default:
+            return state;
+    }
 };
 
 export default function CounterPage({ initialCount }) {
@@ -16,22 +42,30 @@ export default function CounterPage({ initialCount }) {
     });
 
     const increment = () => {
-        dispatch();
+        dispatch({
+            type: INCREMENT_COUNT
+        });
     };
 
     const decrement = () => {
-        // setCount(count - 1);
+        dispatch({
+            type: DECREMENT_COUNT
+        });
     };
 
     const handleChange = (event) => {
         const value = parseInt(event.target.value) || 0;
-        // setValueToAdd(value);
+        dispatch({
+            type: SET_VALUE_TO_ADD,
+            payload: value
+        });
     };
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        // setCount(count + valueToAdd)
-        // setValueToAdd(0);
+        dispatch({
+            type: ADD_VALUE_TO_COUNT
+        });
     };
 
     return (
