f164b1ce30192417a2316722c85805b5ea06e7c4
Arth Limchiu <limchiuarth@gmail.com>
2024-03-26 21:05:42 +0800

    Refactor folder structure to organize by functions which prevents circular imports common to redux toolkit

--- React/rtk/src/App.js
+++ React/rtk/src/App.js
@@ -2,7 +2,7 @@ import './styles.css';
 import { useDispatch } from 'react-redux';
 import MoviePlaylist from './components/MoviePlaylist';
 import SongPlaylist from './components/SongPlaylist';
-import { reset } from './store';
+import { reset } from './store/actions';
 
 export default function App() {
   const dispatch = useDispatch();
