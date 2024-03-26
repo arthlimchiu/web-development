593cf9feaaa44f7f8e980c380f5c15766825bcc8
Arth Limchiu <limchiuarth@gmail.com>
2024-03-26 20:26:26 +0800

    Add redux toolkit integration for Song and Movie Playlist components with useDispatch, and useSelector functions

--- React/rtk/src/store/index.js
+++ React/rtk/src/store/index.js
@@ -8,16 +8,33 @@ const songsSlice = createSlice({
             state.push(action.payload);
         },
         removeSong(state, action) {
+            const index = state.indexOf(action.payload);
+            state.splice(index, 1);
+        }
+    }
+});
 
+const moviesSlice = createSlice({
+    name: 'movie',
+    initialState: [],
+    reducers: {
+        addMovie(state, action) {
+            state.push(action.payload);
+        },
+        removeMovie(state, action) {
+            const index = state.indexOf(action.payload);
+            state.splice(index, 1);
         }
     }
 });
 
 const store = configureStore({
     reducer:  {
-        songs: songsSlice.reducer
+        songs: songsSlice.reducer,
+        movies: moviesSlice.reducer
     }
 });
 
 export { store };
-
+export const { addSong, removeSong } = songsSlice.actions;
+export const { addMovie, removeMovie } = moviesSlice.actions;
