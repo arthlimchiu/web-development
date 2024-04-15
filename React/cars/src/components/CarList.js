e52668be35f4d675ca89ba639dd11595dec05c4a
Arth Limchiu <limchiuarth@gmail.com>
2024-04-15 21:44:52 +0800

    Remove double keys

--- React/cars/src/components/CarList.js
+++ React/cars/src/components/CarList.js
@@ -1,3 +1,9 @@
+import { useSelector } from 'react-redux';
+
 export default function CarList() {
+    const cars = useSelector((state) => {
+        return state.cars.data;
+    });
+
     return (<div>CarList</div>)
 }
