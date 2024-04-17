c4cc64b1f546cc861f580bc0228251f8baa9018a
Arth Limchiu <limchiuarth@gmail.com>
2024-04-17 21:20:35 +0800

    Initial integration for fetch users thunk

--- React/media/src/components/UsersList.js
+++ React/media/src/components/UsersList.js
@@ -1,4 +1,14 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchUsers } from '../store';
+
 export default function UsersList() {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchUsers());
+    }, []);
+
     return (
         <div>Users List</div>
     );
