b2ff7cb39f2b7d052c91cc6e9bc0d362faba364e
Arth Limchiu <limchiuarth@gmail.com>
2024-04-17 22:46:19 +0800

    Add loading to content only and not full screen

--- React/media/src/components/UsersList.js
+++ React/media/src/components/UsersList.js
@@ -15,44 +15,39 @@ export default function UsersList() {
 
     useEffect(() => {
         doFetchUsers();
-    }, []);
+    }, [doFetchUsers]);
 
     const handleUserAdd = () => {
         doCreateUser();
     };
 
+    let content;
     if (isLoadingUsers) {
-        return <Skeleton times={6} className="h-10 w-full"></Skeleton>;
-    }
-
-    if (loadingUsersError) {
-        return <div>Error fetching data...</div>
-    }
-
-    const renderedUsers = data.map((user) => {
-        return (
-            <div key={user.id} className="mb-2 border rounded">
-                <div className="flex p-2 justify-between items-center cursor-pointer">
-                    {user.name}
+        content = <Skeleton times={6} className="h-10 w-full"></Skeleton>;
+    } else if (loadingUsersError) {
+        content = <div>Error fetching data...</div>
+    } else {
+        content = data.map((user) => {
+            return (
+                <div key={user.id} className="mb-2 border rounded">
+                    <div className="flex p-2 justify-between items-center cursor-pointer">
+                        {user.name}
+                    </div>
                 </div>
-            </div>
-        );
-    });
+            );
+        });
+    }
 
     return (
         <div>
-            <div className="flex flex-row justify-between m-3">
+            <div className="flex flex-row justify-between items-center m-3">
                 <h1 className="m2 text-xl">Users</h1>
-                {
-                    isCreatingUser ?
-                    'Creating User...' :
-                    <Button onClick={handleUserAdd}>
-                        + Add User
-                    </Button>
-                }
+                <Button loading={isCreatingUser} onClick={handleUserAdd}>
+                    + Add User
+                </Button>
                 { creatingUserError && 'Error creating user...'}
             </div>
-            {renderedUsers}
+            {content}
         </div>
     );
 }
