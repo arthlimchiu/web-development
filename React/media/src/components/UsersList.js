a7cb4ba21a69330bdd57ce169d9c9a699588686c
Arth Limchiu <limchiuarth@gmail.com>
2024-04-17 21:24:26 +0800

    Add initial state handling using useSelector in users list component

--- React/media/src/components/UsersList.js
+++ React/media/src/components/UsersList.js
@@ -1,15 +1,27 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchUsers } from '../store';
 
 export default function UsersList() {
     const dispatch = useDispatch();
 
+    const { isLoading, data, error } = useSelector((state) => {
+        return state.users;
+    });
+
     useEffect(() => {
         dispatch(fetchUsers());
     }, []);
 
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    if (error) {
+        return <div>Error fetching data...</div>
+    }
+
     return (
-        <div>Users List</div>
+        <div>{data.length}</div>
     );
 }
