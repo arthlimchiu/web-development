3a0d81645712285c5737c825f415ebfe3b37ebb4
Arth Limchiu <limchiuarth@gmail.com>
2024-04-18 16:06:28 +0800

    Initial integration for add album rtk query api

--- React/media/src/store/index.js
+++ React/media/src/store/index.js
@@ -19,4 +19,4 @@ setupListeners(store.dispatch);
 export * from './thunks/fetchUsers';
 export * from './thunks/addUser';
 export * from './thunks/removeUser';
-export { useFetchAlbumsQuery } from './apis/albumApi';
+export { useFetchAlbumsQuery, useAddAlbumMutation } from './apis/albumApi';
