7917764b9b0d45eb00a86a38ead8b886d0196a0b
Arth Limchiu <limchiuarth@gmail.com>
2024-04-18 19:24:22 +0800

    Integration for remove album using rtk query api with better tag design for invalidation

--- React/media/src/store/apis/albumApi.js
+++ React/media/src/store/apis/albumApi.js
@@ -4,14 +4,33 @@ import { faker } from '@faker-js/faker';
 const albumsApi = createApi({
     reducerPath: 'albums',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'http://localhost:3005'
+        baseUrl: 'http://localhost:3005',
+        fetchFn: (...args) => {
+            // Do something here for every request and then return
+            // to continue fetch
+            return fetch(...args);
+        }
     }),
     endpoints(builder) {
         return {
+            removeAlbum: builder.mutation({
+                invalidatesTags: (result, error, album) => {
+                    return [{ type: 'Album', id: album.id }];
+                },
+                query: (album) => {
+                    return {
+                        url: `/albums/${album.id}`,
+                        method: 'DELETE'
+                    };
+                }
+            }),
             addAlbum: builder.mutation({
+                invalidatesTags: (result, error, user) => {
+                    return [{ type: 'UsersAlbums', id: user.id }];
+                },
                 query: (user) => {
                     return {
-                        url: 'albums',
+                        url: '/albums',
                         method: 'POST',
                         body: {
                             userId: user.id,
@@ -21,6 +40,13 @@ const albumsApi = createApi({
                 }
             }),
             fetchAlbums: builder.query({
+                providesTags: (result, error, user) => {
+                    const tags = result.map(album => {
+                        return { type: 'Album', id: album.id };
+                    });
+                    tags.push({ type: 'UsersAlbums', id: user.id });
+                    return tags;
+                },
                 query: (user) => {
                     return {
                         url: '/albums',
@@ -35,5 +61,5 @@ const albumsApi = createApi({
     }
 });
 
-export const { useFetchAlbumsQuery, useAddAlbumMutation } = albumsApi;
+export const { useFetchAlbumsQuery, useAddAlbumMutation, useRemoveAlbumMutation } = albumsApi;
 export { albumsApi };
--- React/media/src/store/index.js
+++ React/media/src/store/index.js
@@ -19,4 +19,4 @@ setupListeners(store.dispatch);
 export * from './thunks/fetchUsers';
 export * from './thunks/addUser';
 export * from './thunks/removeUser';
-export { useFetchAlbumsQuery, useAddAlbumMutation } from './apis/albumApi';
+export { useFetchAlbumsQuery, useAddAlbumMutation, useRemoveAlbumMutation } from './apis/albumApi';
