a974ed654cbffaca6f79269820c71a5068aab8c7
Arth Limchiu <limchiuarth@gmail.com>
2024-04-20 22:28:17 +0800

    Add initial components

--- React/location-search/src/App.tsx
+++ React/location-search/src/App.tsx
@@ -1,6 +1,16 @@
+import Map from './components/Map';
+import LocationSearch from './components/LocationSearch';
+
 function App() {
   return (
-    <div className="m-4">Hi there!</div>
+    <div className="h-screen w-screen grid grid-cols-12">
+      <div className="col-span-3 p-2">
+        <LocationSearch />
+      </div>
+      <div className="col-span-9">
+        <Map />
+      </div>
+    </div>
   )
 }
 
---
+++ React/location-search/src/components/LocationSearch.tsx
@@ -0,0 +1,5 @@
+export default function LocationSearch() {
+    return (
+        <div>Search for a location!</div>
+    );
+}
