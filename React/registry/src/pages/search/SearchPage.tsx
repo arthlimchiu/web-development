d24f7554eb3591311ff43aaef499d29696850708
Arth Limchiu <limchiuarth@gmail.com>
2024-04-22 14:47:49 +0800

    Finalized search page

--- React/registry/src/pages/search/SearchPage.tsx
+++ React/registry/src/pages/search/SearchPage.tsx
@@ -1,11 +1,21 @@
+
+import type { SearchLoaderResult } from './searchLoader';
 import { useLoaderData } from 'react-router-dom';
+import PackageListItem from '../../components/PackageListItem';
 
 export default function SearchPage() {
-    const data = useLoaderData();
+    const { searchResults } = useLoaderData() as SearchLoaderResult;
 
-    console.log(data);
+    const renderedResults = searchResults.map(result => {
+        return <PackageListItem pack={result} key={result.name} />
+    });
 
     return (
-        <div>Search Page</div>
+        <div>
+            <h1 className="text-2xl font-bold my-6">Search Results</h1>
+            <div className="space-y-4 mt-4">
+                {renderedResults}
+            </div>
+        </div>
     );
 }
